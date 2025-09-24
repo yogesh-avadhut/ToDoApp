9476deb8b41c69f249363a943b8c4d31aa65ed79
yogesh-avadhut <dev.yogeshavadhut@gmail.com>
2025-09-25 05:29:02 +0530

    adding and listing api created and resolve errors with cors

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,42 @@
 import e from "express";
-import  { connection, collectionName } from "./dbconfig.js"
+import { connection, collectionName } from "./dbconfig.js"
 const app = e();
-
+import cors from "cors";
 
 app.use(e.json());
-app.post("/add-task",async (req,resp)=>{
+app.use(cors());
+app.post("/add-task", async (req, resp) => {
     const db = await connection();
     const collection = await db.collection(collectionName);
     const result = await collection.insertOne(req.body);
-    resp.send("working ...")
+    if (result) {
+        resp.send({ message: "new task added ...", success: true, result })
+    }
+    else {
+        resp.send({ message: "new task not added ...", success: false, result })
+    }
+})
+
+app.get("/tasks", async (req, resp) => {
+    const db = await connection();
+    const collection = await db.collection(collectionName);
+    const result = await collection.find().toArray();
+
+    if (result) {
+        resp.send({ message: 'task list featched', success: true, result })
+    }
+    else {
+        resp.send({ message: 'task list not featched', success: false })
+
+    }
+
 })
 
 
-app.get("/",(req,resp)=> {
-    resp.send({
-        message:"basic api done ...",
-        success: true
-    })
-})
+
+
+
+
+
 
 app.listen(3200)
