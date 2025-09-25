9be831c6bcd5481224f3083de8e0b70b8619a9b2
yogesh-avadhut <dev.yogeshavadhut@gmail.com>
2025-09-26 03:01:23 +0530

    added multiple delete api

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,8 @@ app.post("/add-task", async (req, resp) => {
     }
 })
 
+
+//get all tasks 
 app.get("/tasks", async (req, resp) => {
     const db = await connection();
     const collection = await db.collection(collectionName);
@@ -31,21 +33,83 @@ app.get("/tasks", async (req, resp) => {
     }
 })
 
-
-app.delete("/delete/:id", async(req,resp)=>{
+//delete task by id 
+app.delete("/delete/:id", async (req, resp) => {
     const db = await connection();
-   const id =  req.params.id
+    const id = req.params.id
     const collection = await db.collection(collectionName);
-    const result = await collection.deleteOne({_id:new ObjectId(id)})
+    const result = await collection.deleteOne({ _id: new ObjectId(id) })
 
-    if (result){
-        resp.send({message: 'task deleted', success: true, result})
+    if (result) {
+        resp.send({ message: 'task deleted', success: true, result })
     }
     else {
-        resp.send({message:'deletion failed', success:false})
+        resp.send({ message: 'deletion failed', success: false })
     }
 
 })
+
+
+//delete multiple 
+app.delete("/delete-multiple", async (req, resp) => {
+    const db = await connection();
+    const ids = req.body
+    const deleteTaskIds = ids.map((item) => new ObjectId(item))
+    const collection = await db.collection(collectionName);
+    
+    const result = await collection.deleteMany({ _id: { $in: deleteTaskIds } })
+
+    if (result) {
+        resp.send({ message: 'multiple task deleted', success: true , result})
+    }
+    else {
+        resp.send({ message: 'multiple delete failed', success: false })
+    }
+
+})
+
+
+
+
+
+
+//get task by id 
+app.get("/task/:id", async (req, resp) => {
+    const db = await connection();
+    const collection = await db.collection(collectionName);
+    const id = req.params.id
+    const result = await collection.findOne({ _id: new ObjectId(id) })
+
+    if (result) {
+        resp.send({ message: 'task featched', success: true, result })
+    }
+    else {
+        resp.send({ message: 'task not featched', success: false })
+    }
+})
+
+
+// update task by id 
+app.put("/update-task", async (req, resp) => {
+    const db = await connection();
+    const collection = db.collection(collectionName);
+    const { _id, ...fields } = req.body
+
+    const result = await collection.updateOne(
+        { _id: new ObjectId(_id) },
+        { $set: fields }
+    );
+    
+    if (result) {
+        resp.send({ message: 'task list updated', success: true, result })
+    }
+    else {
+        resp.send({ message: 'task list not updated', success: false })
+    }
+})
+
+
+
 
 
 
